18f2a9e6d6621e7bff2e67612e225d93f47425b1
lisCloud <lgutierrez@cloudgeometry.com>
2025-06-04 23:45:46 +0200

    real notes

--- src/notes.ts
+++ src/notes.ts
@@ -1,10 +1,18 @@
 export const notes = [
     {
-        author: 'John Doe',
-        note: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        author: 'Arnel',
+        note: "Haci, it was great to meet you in person in Istanbul. It's been great working with you. I wish you the best on the new team. I'm glad you're still nearby us. Bye for now."
     },
     {
-        author: 'Jane Doe',
-        note: 'This is another sample note.',
+        author: 'Alyona',
+        note: "It's a bittersweet moment to see you move on to a new team. 😢 Working with you has been an absolute pleasure ☀️ - your professionalism, calm presence, and supportive spirit have made a real difference here. While I'll definitely miss having you on our team 🫶🏻, I'm genuinely happy that you're taking on new challenges and opportunities.🚀"
+    },
+    {
+        author: 'Pedro Tramontina',
+        note: 'I really enjoyed working with Haci. Not only by his technical knowledge, but for his personality. He was always willing to help and making everything on his power to make our systems better.'
+    },
+    {
+        author: 'Liseth',
+        note: "Haci! I'm going to miss you. Your commitment and help was invaluable. I wish you all the best in your new team, and I hope we can stay in touch! 💖",
     }
 ]
